fb0ffee0caa283f211123056c6270dcff649bc0d
Sagar Gajare <sggajare360@gmail.com>
2023-10-08 06:21:04 +0000

    feat: remove chakra ui provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,10 @@ import "katex/dist/katex.min.css";
 
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
   );
 }
 
